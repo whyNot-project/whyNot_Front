d0a907487e138a0869c8d373ec0b80d3d490d12b
yeyeah1 <2yeyeah1@gmail.com>
2023-11-20 16:57:11 +0900

    [feat] 전체 크루 보기, 크루 상세, 크루 생성 폼  추가

--- src/components/crew/crewDetail.vue
+++ src/components/crew/crewDetail.vue
@@ -1,13 +1,31 @@
 <template>
-    <div>
-  
-    </div>
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped>
-  
-  </style>
+  <div>
+    <h3>크루 상세</h3>
+    <v-card variant="outlined">
+      <v-card-title>{{ crews.crewName }}</v-card-title>
+      <v-card-subtitle>{{ crews.schedule }}</v-card-subtitle>
+      <v-card-text>{{ crews.content }}</v-card-text>
+
+      <v-card-actions>
+        <v-btn @click="$router.push('registCrew')">가입하기</v-btn>
+      </v-card-actions>
+    </v-card>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, computed } from "vue";
+import { useCrewStore } from "@/stores/Crew";
+import { useRoute } from "vue-router";
+
+const crewStore = useCrewStore();
+const route = useRoute();
+
+let crews = computed(() => crewStore.crewDetail);
+
+onMounted(() => {
+  crewStore.getCrewDetail();
+});
+</script>
+
+<style scoped></style>
--- src/views/RegistCrewView.vue
+++ src/views/RegistCrewView.vue
@@ -1,13 +1,66 @@
 <template>
-    <div>
-  
-    </div>
-  </template>
-  
-  <script setup>
-  
-  </script>
-  
-  <style scoped>
-  
-  </style>
+  <div>
+    <h3>크루 만들기</h3>
+    <v-sheet width="300" class="mx-auto">
+      <v-form fast-fail @submit.prevent>
+        <v-text-field
+          v-model="crewName"
+          label="크루 이름을 입력하세요."
+          :counter="20"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="schedule"
+          label="언제 모이실 예정인가요?"
+          hint="YYYY-MM-DD 형식으로 작성해주세요"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="memberNum"
+          label="몇 명을 모집하고 싶으신가요?"
+        ></v-text-field>
+
+        <v-text-field v-model="tag" label="운동"> </v-text-field>
+
+        <v-text-field
+          v-model="location"
+          label="모일 장소를 적어주세요."
+        ></v-text-field>
+
+        <v-text-field
+          v-model="content"
+          label="크루를 소개하는 글을 적어주세요."
+          :counter="50"
+        ></v-text-field>
+
+        <v-btn type="submit" block class="mt-2">크루 만들기</v-btn>
+      </v-form>
+    </v-sheet>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+
+const crewName = ref("");
+const schedule = ref("");
+const memberNum = ref("");
+const tag = ref("");
+const location = ref("");
+const content = ref("");
+
+const createCrew = () => {
+  // 이 부분에 실제로 서버로 데이터를 전송하는 로직을 추가해야 합니다.
+  // 현재는 콘솔에 데이터를 출력하는 예시 코드입니다.
+  console.log({
+    crewName: crewName.value,
+    schedule: schedule.value,
+    memberNum: memberNum.value,
+    tag: tag.value,
+    location: location.value,
+    content: content.value,
+  });
+};
+</script>
+
+<style scoped></style>
